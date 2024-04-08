49dacc51ca368b40f05e8c399cf6ecd5d1a3405f
speed4z <shardulshinde2812@gmail.com>
2024-04-09 02:17:24 +0530

    updated with map inline function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import Term from "./Term";
 import emojipedia from "../emojipedia";
 
-function createTerm(emo){
-  return <Term 
-  key = {emo.id}
-  id = {emo.id}
-  emoji = {emo.emoji}
-  name = {emo.name}
-  meaning = {emo.meaning}
-  />
-}
 
 function App() {
   return (
@@ -18,7 +9,17 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">      {emojipedia.map(createTerm)}      </dl>
+      <dl className="dictionary">      
+      {emojipedia.map( (emo) =>{
+        return <Term 
+        key = {emo.id}
+        id = {emo.id}
+        emoji = {emo.emoji}
+        name = {emo.name}
+        meaning = {emo.meaning}
+        />
+      })
+      }</dl>
     </div>
   );
 }
